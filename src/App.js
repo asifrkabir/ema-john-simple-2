ce10a70eb90d22a388622c678e156998158d2824
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-03-19 13:31:45 +0600

    basic routing complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path="/shop" element={<Shop></Shop>}></Route>
+        <Route path="/review" element={<Review></Review>}></Route>
+        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        <Route exact path="/" element={<Shop></Shop>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Sorry, page not found</h1>
+            <h4>Error 404</h4>
+        </div>
+    );
+};
+
+export default NotFound;
